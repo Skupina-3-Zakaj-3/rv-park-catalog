94e304b9709546313d7b71028c85402e8f6244cf
Tadej Stanonik <ts6103@student.uni-lj.si>
2021-12-31 11:49:09 +0100

    Location field added

--- models/src/main/resources/sql-scripts/init.sql
+++ models/src/main/resources/sql-scripts/init.sql
@@ -1,1 +1,1 @@
-INSERT INTO parks (user_id, name, cost_per_day, description) VALUES (1, 'Camp Bled', 35.24, 'Very nice camping place.');
+INSERT INTO parks (user_id, name, cost_per_day, description, location) VALUES (1, 'Camp Bled', 35.24, 'Very nice camping place.', 'Ljubljana');
