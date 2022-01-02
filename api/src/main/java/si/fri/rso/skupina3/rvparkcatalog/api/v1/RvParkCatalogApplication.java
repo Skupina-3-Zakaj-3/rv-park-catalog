e584208e8d2ee305d84f6f91e62e97d94d700d69
Tadej Stanonik <ts6103@student.uni-lj.si>
2022-01-02 22:25:11 +0100

    Service discovery and consul

--- api/src/main/java/si/fri/rso/skupina3/rvparkcatalog/api/v1/RvParkCatalogApplication.java
+++ api/src/main/java/si/fri/rso/skupina3/rvparkcatalog/api/v1/RvParkCatalogApplication.java
@@ -1,8 +1,11 @@
 package si.fri.rso.skupina3.rvparkcatalog.api.v1;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@RegisterService(value="rv-park-catalog-service", environment="dev", version = "1.0.0")
 public class RvParkCatalogApplication extends Application {
 }
